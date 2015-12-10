111af0483ac2f4b4baa9c23a595ddfaa3170a2c0
Ramiro Vera <raalveco@gmail.com>
2015-12-10 16:44:06 -0600

    Package - First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Openpay
+
+Library that will be used to connect to OpenPay Payment Gateway
+
---
+++ src/OpenpayServiceProvider.php
@@ -0,0 +1,23 @@
+<?php namespace Intagono\Openpay;
+
+use Illuminate\Support\ServiceProvider;
+
+class OpenpayServiceProvider extends ServiceProvider {
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //
+    }
+
+}
