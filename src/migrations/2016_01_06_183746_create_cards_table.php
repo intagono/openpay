db9be4ab4c6e16f755dcb5c155ffd9f4cbbb044e
Ramiro Vera <raalveco@gmail.com>
2016-01-07 11:39:27 -0600

    Cards With Database

--- src/migrations/2016_01_06_183746_create_cards_table.php
+++ src/migrations/2016_01_06_183746_create_cards_table.php
@@ -18,6 +18,7 @@ class CreateCardsTable extends Migration
 
             $table->increments('id');
             $table->string('openpay_id');
+            $table->string('customer_id');
 
             $table->string('type');
             $table->string('brand')->nullable();
@@ -28,16 +29,6 @@ class CreateCardsTable extends Migration
             $table->string('expiration_year',2);
             $table->string('bank_name')->nullable();
             $table->string('bank_code')->nullable();
-
-            $table->string('email')->nullable();
-            $table->string('phone_number')->nullable();
-
-            $table->string('street')->nullable();
-            $table->string('colony')->nullable();
-            $table->string('zipcode',5)->nullable();
-            $table->string('city')->nullable();
-            $table->string('state')->nullable();
-            $table->string('country_code',2)->default("MX");
 
             $table->boolean('status')->default(true);
 
--- src/models/Card.php
+++ src/models/Card.php
@@ -3,5 +3,5 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model {
-    protected $fillable = array('openpay_id', 'type', 'brand', 'holder_name', 'card_number', 'expiration_month', 'expiration_year', 'bank_name', 'bank_code', 'email', 'phone_number', 'street', 'colony', 'zipcode', 'city', 'state');
+    protected $fillable = array('openpay_id', 'customer_id', 'type', 'brand', 'holder_name', 'card_number', 'expiration_month', 'expiration_year', 'bank_name', 'bank_code');
 }
