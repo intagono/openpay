6a40f056e79256427f54d0248673ad095fcfef69
Ramiro Vera <raalveco@gmail.com>
2016-01-21 17:52:47 -0600

    Error messages added in the OpenpayExceptionTrait

--- src/OpenpayExceptionTrait.php
+++ src/OpenpayExceptionTrait.php
@@ -31,11 +31,19 @@ trait OpenpayExceptionTrait {
     {
         if ($e instanceof OpenpayApiTransactionError)
         {
-            $message = 'ERROR en la transacción: ' . $e->getMessage()
-                     . ' [código de error: ' . $e->getErrorCode()
-                     . ', categoría de error: ' . $e->getCategory()
-                     . ', código HTTP: ' . $e->getHttpCode()
-                     . ', id petición: ' . $e->getRequestId() . ']';
+            switch($e->getErrorCode()){
+                case 3001: $message = "La tarjeta fue rechazada."; break;
+                case 3002: $message = "La tarjeta ha expirado."; break;
+                case 3003: $message = "La tarjeta no tiene fondos suficientes."; break;
+                case 3004: $message = "La tarjeta ha sido identificada como una tarjeta robada."; break;
+                case 3005: $message = "La tarjeta ha sido identificada como una tarjeta fraudulenta."; break;
+                default:
+                    $message = 'ERROR en la transacción: ' . $e->getMessage()
+                        . ' [código de error: ' . $e->getErrorCode()
+                        . ', categoría de error: ' . $e->getCategory()
+                        . ', código HTTP: ' . $e->getHttpCode()
+                        . ', id petición: ' . $e->getRequestId() . ']';
+            }
         }
         elseif ($e instanceof OpenpayApiRequestError)
         {
